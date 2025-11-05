27fbbeadec156bebcb39517889cd42b30d932eb3
suyashrcb15 <142603779+suyashrcb15@users.noreply.github.com>
2025-11-05 14:18:32 +0530

    Updated frontend and backend files with fixes and cleanup

--- ecom-frontend-5-main/src/axios.jsx
+++ ecom-frontend-5-main/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api",
+  baseURL: "https://shopeasy8-0.onrender.com/api",
 });
-delete API.defaults.headers.common["Authorization"];
+
 export default API;
--- ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class ProductController {
 
